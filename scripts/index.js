33f233e220ca2fee044cf068d3d7cefc96634afc
Margarita Sergeeva <margoriii.direct@yandex.ru>
2024-07-04 16:53:11 +0300

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard({name, link}, onDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__image').src =link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  deleteButton.addEventListener('click', onDelete)
+  return cardElement;
+}
+
 
 // @todo: Функция удаления карточки
+function handDeleteCard (evt) {
+  evt.target.closest('.card').remove()
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((dataCard) => {
+  placesList.append(createCard(dataCard, handDeleteCard));
+})
+
